719c7ff9ebdef7c72f1a95c898e1c6a3e624a852
PriyaBihani <priyabihani.official@gmail.com>
2024-01-08 01:17:55 +0530

    auth flow

---
+++ server/controller/auth.js
@@ -0,0 +1,94 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const signup = async (req, res) => {
+  try {
+    if (req.cookies.access_token) {
+      return res.redirect("/todos");
+    }
+    const { email, username, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        status: "error",
+        msg: "User already exists",
+        data: null,
+      });
+    }
+
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      status: "success",
+      msg: "User created successfully",
+      data: newUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "error",
+      msg: "Internal server error",
+      data: null,
+    });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    if (req.cookies.access_token) {
+      return res.redirect("/todos");
+    }
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        msg: "User not found",
+        data: null,
+      });
+    }
+
+    // Authenticate user - verify password
+    if (user.password !== password) {
+      return res.status(400).json({
+        status: "error",
+        msg: "Incorrect password",
+        data: null,
+      });
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: false,
+      })
+      .status(200)
+      .json({
+        status: "success",
+        msg: "User logged in successfully",
+        data: {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+        },
+      });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      msg: "Internal server error",
+    });
+  }
+};
+
+module.exports = { signup, login };
--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const { connectDB } = require("./utils/db.utils");
+const authRouter = require("./routes/auth.js");
 
 const app = express();
+const PORT = process.env.PORT || 8000;
+
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
