14dcacb7ff0bc16776af7ff7e8519f94634fed79
PriyaBihani <priyabihani.official@gmail.com>
2024-01-23 17:01:52 +0530

    start files

---
+++ scripts/application_start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#give permission for everything in the api directory
+sudo chmod -R 777 /home/ec2-user/MERN_final_project_insta_clone
+
+#navigate into our working directory where we have all our github files
+cd /home/ec2-user/MERN_final_project_insta_clone/server
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+npm i -g pm2
+
+#install node modules
+npm install
+
+#start our node app in the background
+pm2 start index.js --name "insta_clone"
+
+echo "Started the file"
